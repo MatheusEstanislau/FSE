2eff792cb7a3abd9de0341f7f5a3e08d66f5d5df
MatheusEstanislau <matheus.estanislau7@gmail.com>
2021-03-21 15:50:03 -0300

    refactor: remove unused directories

--- i2c/i2c.h
+++
@@ -1,6 +0,0 @@
-#ifndef __I2C__
-#define __I2C__
-
-void writeInLcd(float tr, float ti, float te);
-void ClrLcd();
-#endif // __I2C__
--- uartModBus/uart.h
+++
@@ -1,8 +0,0 @@
-#ifndef __UART__
-#define __UART__
-
-float getPotentiometerData();
-float getLM35Data();
-void initializeUart();
-
-#endif // __UART__
