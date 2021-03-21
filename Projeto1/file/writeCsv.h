00fb77f46c4450feb56bfc3489f5046579b78f05
MatheusEstanislau <matheus.estanislau7@gmail.com>
2021-03-21 15:07:07 -0300

    refactor: add project1 to your own directory

---
+++ Projeto1/file/writeCsv.h
@@ -0,0 +1,7 @@
+#ifndef __WRITEINCSV__
+#define __WRITEINCSV__
+
+void initWrite();
+void writeInCsv(float text, float tint, float tref, float cooler, float resistor);
+
+#endif // __WRITEINCSV__
---
+++ Projeto1/i2c/i2c.h
@@ -0,0 +1,6 @@
+#ifndef __I2C__
+#define __I2C__
+
+void writeInLcd(float tr, float ti, float te);
+void ClrLcd();
+#endif // __I2C__
