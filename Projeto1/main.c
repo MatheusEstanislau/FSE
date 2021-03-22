cceb3541b4bcd4026971074af703408c97c74f18
MatheusEstanislau <matheus.estanislau7@gmail.com>
2021-03-22 13:06:01 -0300

    feat: add menu when user interrupts system

--- Projeto1/main.c
+++ Projeto1/main.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
 #include "bme280/bme280.h"
 #include "uartModBus/uart.h"
-#include <unistd.h>
 #include "i2c/i2c.h"
 #include "pwm/wiringPi.h"
 #include "pid/pid.h"
 #include "file/writeCsv.h"
-#include <signal.h>
 
-float ventoinha;
+float cooler;
 float resistor;
-int flagStop = 0;
+int writeCounter = 0;
+float userTemperature;
 float referenceTemperature = 0;
-float user;
-int escrita = 0;
+float potentiometer, lm35;
+int switchMode = 0;
 void poweroff()
 {
   printf("\nDesligando Sistema\n");
@@ -28,32 +29,39 @@ void poweroff()
 void waitFunction()
 {
   alarm(1);
-  if (escrita == 2)
+  if (writeCounter == 2)
   {
-    escrita = 0;
+    writeCounter = 0;
   }
-  escrita++;
+  writeCounter++;
 }
 
 void userInsertStop()
 {
-  if (flagStop == 0)
+
+  printf("\nQual modo de referência deseja utilizar?\n");
+  printf("Digite 0, para potenciometro\n");
+  printf("Digite 1 para entrar com sua temperatura\n");
+  scanf("%d", &switchMode);
+  switch (switchMode)
   {
-    flagStop = 1;
+  case 0:
+    printf("Modo escolhido: Potenciometro\n");
+    referenceTemperature = potentiometer;
+    break;
+  case 1:
     printf("\nInsira a temperatura desejada: \n");
-    scanf("%f", &user);
-  }
-  else
-  {
-    printf("\nIniciando modo com Potenciometro\n");
-    flagStop = 0;
+    scanf("%f", &userTemperature);
+    printf("Alterando para temperatura escolhida: %f\n", userTemperature);
+  default:
+    break;
   }
 }
 
 int main(int argc, char const *argv[])
 {
   int initialize;
-  float potentiometer, lm35;
+
   int Temperature, Pressure, Humidity;
   wiringPiSetup();
   initWrite();
@@ -67,13 +75,13 @@ int main(int argc, char const *argv[])
   {
     signal(SIGTSTP, userInsertStop);
     potentiometer = getPotentiometerData();
-    if (flagStop == 0)
+    if (switchMode == 0)
     {
       referenceTemperature = potentiometer;
     }
     else
     {
-      referenceTemperature = user;
+      referenceTemperature = userTemperature;
     }
     lm35 = getLM35Data();
     bme280ReadValues(&Temperature, &Pressure, &Humidity);
@@ -86,13 +94,13 @@ int main(int argc, char const *argv[])
       activateResistor(controle);
       disableCooler();
       resistor = controle;
-      ventoinha = 0;
+      cooler = 0;
     }
     else if (controle < -40)
     {
       activateCooler((controle * -1));
       disableResistor();
-      ventoinha = controle * -1;
+      cooler = controle;
       resistor = 0;
     }
     else
@@ -103,22 +111,22 @@ int main(int argc, char const *argv[])
     printf("\n|-----------------------------------------------------------|\n");
     printf("|                                                           |\n");
     printf("|     Text = %.2f ºC, Tint. = %.2f ºC  Tref = %.2f ºC    |\n", (float)Temperature / 100, lm35, referenceTemperature);
-    printf("|       Uso Ventoinha: %.2f %% Uso Resistor: %.2f %%         |\n", ventoinha, resistor);
+    printf("|         Uso cooler: %.2f %% Uso Resistor: %.2f %%         |\n", cooler, resistor);
     printf("|  digite: ctrl + z para alterar modo de temperatura        |\n");
-    if (flagStop == 0)
+    if (switchMode == 0)
     {
-      printf("|                  modo Potenciometro                       |\n");
+      printf("|              Referencia: Potenciometro                     |\n");
     }
     else
     {
-      printf("|                     modo Usuario                          |\n");
+      printf("|                  Referencia: Usuario                         |\n");
     }
-    printf("|            digite: ctrl + c para encerrar                 |\n");
+    printf("|            Digite: ctrl + c para encerrar                 |\n");
     printf("|-----------------------------------------------------------|\n");
     writeInLcd(referenceTemperature, lm35, (float)Temperature / 100);
-    if (escrita == 2)
+    if (writeCounter == 2)
     {
-      writeInCsv((float)Temperature / 100, (float)lm35, (float)referenceTemperature, ventoinha, resistor);
+      writeInCsv((float)Temperature / 100, (float)lm35, (float)referenceTemperature, cooler, resistor);
     }
     pause();
   }
